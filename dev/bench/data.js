eb000f1347bb2d1cd5cf386dc798190d1c4b88ce
github-action-benchmark <github@users.noreply.github.com>
2022-12-08 19:54:43 +0000

    add JMH Benchmark (jmh) benchmark result for 04daf2865c58fa6d93d84454e08e28bb184305d2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670529262230,
+  "lastUpdate": 1670529283254,
   "repoUrl": "https://github.com/j3hempsey/github-action-benchmark",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -2182,6 +2182,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "org.openjdk.jmh.samples.JMHSample_01_HelloWorld.wellHelloThere",
             "value": 1379709362.1156056,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jhempsey33@gmail.com",
+            "name": "Justin Hempsey",
+            "username": "j3hempsey"
+          },
+          "committer": {
+            "email": "jhempsey33@gmail.com",
+            "name": "Justin Hempsey",
+            "username": "j3hempsey"
+          },
+          "distinct": false,
+          "id": "04daf2865c58fa6d93d84454e08e28bb184305d2",
+          "message": "Stop publishing node_modules",
+          "timestamp": "2022-12-08T11:32:35-08:00",
+          "tree_id": "80a953358bda41f868d14eef37d3b04b8d83cdec",
+          "url": "https://github.com/j3hempsey/github-action-benchmark/commit/04daf2865c58fa6d93d84454e08e28bb184305d2"
+        },
+        "date": 1670529282519,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.openjdk.jmh.samples.JMHSample_01_HelloWorld.wellHelloThere",
+            "value": 1383578673.0676877,
             "unit": "ops/s",
             "extra": "iterations: 3\nforks: 1\nthreads: 1"
           }
