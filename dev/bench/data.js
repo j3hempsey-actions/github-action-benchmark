dfed4a48cd10a627e96717165f2da3a198fac163
github-action-benchmark <github@users.noreply.github.com>
2022-12-08 22:30:17 +0000

    add Rust Benchmark (cargo) benchmark result for 66b46001278102ec087eac0f22103e7e8e68ab8f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670536525155,
+  "lastUpdate": 1670538617925,
   "repoUrl": "https://github.com/j3hempsey/github-action-benchmark",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -2989,6 +2989,42 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci/Iterative/21",
             "value": 7,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jhempsey33@gmail.com",
+            "name": "Justin Hempsey",
+            "username": "j3hempsey"
+          },
+          "committer": {
+            "email": "jhempsey33@gmail.com",
+            "name": "Justin Hempsey",
+            "username": "j3hempsey"
+          },
+          "distinct": true,
+          "id": "66b46001278102ec087eac0f22103e7e8e68ab8f",
+          "message": "Wrap pr comment in config",
+          "timestamp": "2022-12-08T14:26:37-08:00",
+          "tree_id": "8a554f3d87cf0c95b8b273af6869e39a126a2173",
+          "url": "https://github.com/j3hempsey/github-action-benchmark/commit/66b46001278102ec087eac0f22103e7e8e68ab8f"
+        },
+        "date": 1670538617306,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 178,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 23970,
+            "range": "± 119",
             "unit": "ns/iter"
           }
         ]
