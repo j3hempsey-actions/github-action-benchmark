309c75b3db4d0a9fdcbb37c2f87c697891ae49e8
github-action-benchmark <github@users.noreply.github.com>
2022-12-08 18:19:13 +0000

    add Rust Benchmark (cargo) benchmark result for 43d27241c3d45c35b4e62200059c88a2d195eb59

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670523539164,
+  "lastUpdate": 1670523553590,
   "repoUrl": "https://github.com/j3hempsey/github-action-benchmark",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -1472,6 +1472,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "Fibonacci/Iterative/21",
             "value": 8,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "j3hempsey@users.noreply.github.com",
+            "name": "Justin",
+            "username": "j3hempsey"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "43d27241c3d45c35b4e62200059c88a2d195eb59",
+          "message": "Merge pull request #1 from j3hempsey/test\n\nAdd pr logging",
+          "timestamp": "2022-12-08T10:14:32-08:00",
+          "tree_id": "ef133688b0757fd712e9b150ef21be8f06fdc263",
+          "url": "https://github.com/j3hempsey/github-action-benchmark/commit/43d27241c3d45c35b4e62200059c88a2d195eb59"
+        },
+        "date": 1670523552894,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "BenchFib10",
+            "value": 215,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "BenchFib20",
+            "value": 26769,
+            "range": "± 21",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/20",
+            "value": 26785,
+            "range": "± 22",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/20",
+            "value": 8,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/21",
+            "value": 43377,
+            "range": "± 24",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/21",
+            "value": 9,
             "range": "± 0",
             "unit": "ns/iter"
           }
